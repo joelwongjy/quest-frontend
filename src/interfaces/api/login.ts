6e397ae98d76cdb2d9903ad7de53c294bf6c3ed0
Hanming Zhu <hanming@huawen.com>
2020-09-06 16:25:25 +0800

    fix: remove refresh token import

--- src/interfaces/api/login.ts
+++ src/interfaces/api/login.ts
@@ -1,6 +1,5 @@
-import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from 'utils/tokenUtils';
+import { ACCESS_TOKEN_KEY } from 'utils/tokenUtils';
 
 export default interface LoginData {
   [ACCESS_TOKEN_KEY]: string;
-  [REFRESH_TOKEN_KEY]: string;
 }
