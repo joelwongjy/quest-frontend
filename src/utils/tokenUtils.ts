20cc4d41c795033d95523bc442596df5c5e5f24e
Hanming Zhu <hanming@huawen.com>
2020-09-06 14:09:25 +0800

    feat: fix up utils to work with new backend

--- src/utils/tokenUtils.ts
+++ src/utils/tokenUtils.ts
@@ -2,22 +2,25 @@ import { AxiosResponse } from 'axios';
 
 import LoginData from 'interfaces/api/login';
 
-export const TOKEN_KEY = 'authToken';
+export const ACCESS_TOKEN_KEY = 'accessToken';
+export const REFRESH_TOKEN_KEY = 'refreshToken';
 
-const getToken = (): string | null => {
-  return localStorage.getItem(TOKEN_KEY);
+const getAccessToken = (): string | null => {
+  return localStorage.getItem(ACCESS_TOKEN_KEY);
 };
 
 const storeToken = (response: AxiosResponse<LoginData>): Promise<null> => {
-  if (response.status === 200 || response.status === 201) {
-    localStorage.setItem(TOKEN_KEY, response.data[TOKEN_KEY]);
+  if (response.status === 200) {
+    localStorage.setItem(ACCESS_TOKEN_KEY, response.data[ACCESS_TOKEN_KEY]);
+    localStorage.setItem(REFRESH_TOKEN_KEY, response.data[REFRESH_TOKEN_KEY]);
     return Promise.resolve(null);
   }
   return Promise.reject(new Error(response.statusText));
 };
 
 const removeToken = (): void => {
-  localStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(ACCESS_TOKEN_KEY);
+  localStorage.removeItem(REFRESH_TOKEN_KEY);
 };
 
-export default { getToken, storeToken, removeToken };
+export default { getToken: getAccessToken, storeToken, removeToken };
