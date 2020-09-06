0e12e5ab814a70b45ab438cde188647d4ecee8dd
Hanming Zhu <hanming@huawen.com>
2020-09-06 14:08:07 +0800

    feat: initialise home and login route

---
+++ src/routes/home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Home.scss';
+
+const Home: React.FC = () => {
+  return <div>You&apos;re logged in!</div>;
+};
+
+export default Home;
---
+++ src/routes/login/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Login.scss';
+
+const Login: React.FC = () => {
+  return <></>;
+};
+
+export default Login;
