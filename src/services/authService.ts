18708dbe883cfef155dd42d1d7feb701043ad37e
Hanming Zhu <hanming@huawen.com>
2020-09-06 14:08:47 +0800

    feat: initialise services

---
+++ src/services/authService.ts
@@ -0,0 +1,71 @@
+import { AxiosResponse } from 'axios';
+
+import store from 'app/store';
+import TokenUtils from 'utils/tokenUtils';
+import { setUser, clearUser } from 'reducers/miscDux';
+import ApiService from 'services/apiService';
+import User from 'interfaces/models/users';
+
+const logout = (): Promise<void> => {
+  TokenUtils.removeToken();
+  store.dispatch(clearUser());
+  return Promise.resolve();
+};
+
+const signup = async (
+  username: string,
+  password: string,
+  name: string
+): Promise<null> => {
+  const response = await ApiService.post('users', {
+    username,
+    password,
+    name,
+  }).catch((error) => {
+    return Promise.reject(new Error(error));
+  });
+  // if (response.data.message === 'Account already exists') {
+  //   return Promise.reject(
+  //     new Error('Account already exists! Please login instead.')
+  //   );
+  // }
+  return TokenUtils.storeToken(response);
+};
+
+const login = async (username: string, password: string): Promise<null> => {
+  const response = await ApiService.post('auth/token', {
+    auth: { username, password },
+  }).catch(() => {
+    return Promise.reject(
+      new Error('Invalid login credentials, please try again.')
+    );
+  });
+  return TokenUtils.storeToken(response);
+};
+
+const getUser = async (): Promise<User | null> => {
+  const token = TokenUtils.getToken();
+  if (!token) {
+    return Promise.resolve(null);
+  }
+
+  try {
+    const response = await ApiService.get('auth/me');
+    if (response.status === 200) {
+      const user: User = response.data;
+      store.dispatch(setUser(user));
+      return user;
+    }
+    throw new Error(response.statusText);
+  } catch (error) {
+    logout();
+    return Promise.reject(new Error(error));
+  }
+};
+
+export default {
+  signup,
+  login,
+  logout,
+  getUser,
+};
