f157f27c4deb8e394045c2ce996a4d1d028913ca
Hanming Zhu <hanming@huawen.com>
2020-09-06 14:10:43 +0800

    chore: touch up high level files

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import AppProviders from 'contexts/AppProviders';
+import store, { persistor } from 'app/store';
 import App from './app';
+
 import * as serviceWorker from './serviceWorker';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <AppProviders>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </AppProviders>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
