99ad11ab5b15214cf975a409f496def7b27da2a6
Hanming Zhu <hanming@huawen.com>
2020-10-10 17:38:21 +0800

    fix: fix typings for typescript

---
+++ src/typings/styles.d.ts
@@ -0,0 +1,56 @@
+import { CSSProperties } from '@material-ui/styles';
+
+declare module '@material-ui/core/styles/createMuiTheme' {
+  interface Custom {
+    fontFamily: {
+      roboto: CSSProperties;
+      metropolis: CSSProperties;
+    };
+    icon: {
+      iconColor: string;
+      iconBorderColor: string;
+      iconHighlight: string;
+    };
+  }
+
+  interface CustomOptions {
+    fontFamily?: {
+      roboto?: CSSProperties;
+      metropolis?: CSSProperties;
+    };
+    icon?: {
+      iconColor?: string;
+      iconBorderColor?: string;
+      iconHighlight?: string;
+    };
+  }
+
+  interface Theme {
+    type: string;
+    custom: Custom;
+  }
+
+  // allow configuration using `createMuiTheme`
+  interface ThemeOptions {
+    type?: string;
+    custom?: CustomOptions;
+  }
+}
+
+declare module '@material-ui/core/styles/createMixins' {
+  interface Mixins {
+    drawer: CSSProperties;
+  }
+
+  // allow configuration using `createMuiTheme`
+  interface MixinOptions {
+    drawer?: CSSProperties;
+  }
+}
+
+declare module '@material-ui/core/styles/defaultTheme' {
+  interface DefaultTheme {
+    type: string;
+    custom: Custom;
+  }
+}
