3ef553f3b19abf7926200899da57e5dafa44ecde
Hanming Zhu <hanming@huawen.com>
2020-10-10 17:40:18 +0800

    refactor: restructure component folder and fix theme issues

--- src/components/appBar/searchBar/SearchBar.tsx
+++ src/components/appBar/searchBar/SearchBar.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import {
   Search as SearchIcon,
   CloseOutlined as CloseOutlinedIcon,
@@ -11,57 +10,38 @@ import {
   Snackbar,
   ClickAwayListener,
 } from '@material-ui/core';
-import { useTheme } from '@material-ui/styles';
+import { useTheme } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-  search: {
-    display: 'flex',
-    justifyContent: 'center',
-    maxWidth: '100%',
-    transition: theme.transitions.create('all', {
-      easing: theme.transitions.easing.easeIn,
-      duration: theme.transitions.duration.shortest,
-    }),
-    marginLeft: theme.spacing(1),
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  inputInput: {
-    width: '100%',
-  },
-}));
+import { useStyles } from './searchBar.styles';
 
-const SearchBar = () => {
+const SearchBar: React.FunctionComponent = () => {
   const classes = useStyles();
   const theme = useTheme();
 
   const [isFocused, setFocused] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
   const [isShowingToast, showToast] = useState(false);
+
   const onSearchCancel = () => {
     setSearchTerm('');
     setFocused(false);
   };
-  const onSearch = (event) => {
+
+  const onSearch = (
+    event: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void => {
     setFocused(true);
     if (event.key === 'Enter') {
       showToast(true);
       setFocused(false);
     }
   };
-  const onFocusLoss = () => {
+
+  const onFocusLoss = (): void => {
     setFocused(false);
   };
-  const handleToastClose = () => {
+
+  const handleToastClose = (): void => {
     showToast(false);
   };
 
@@ -73,7 +53,7 @@ const SearchBar = () => {
         bgcolor={
           isFocused
             ? theme.palette.background.default
-            : theme.palette.background.highlight
+            : theme.palette.background.paper
         }
         boxShadow={isFocused ? 2 : 0}
         height="3rem"
@@ -95,7 +75,7 @@ const SearchBar = () => {
         />
         {isFocused ? (
           <IconButton hidden={!isFocused} onClick={onSearchCancel}>
-            <CloseOutlinedIcon htmlColor={theme.custom.palette.iconColor} />
+            <CloseOutlinedIcon htmlColor={theme.custom.icon.iconColor} />
           </IconButton>
         ) : null}
         <Snackbar
