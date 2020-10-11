cf5e672f87ff74c2bc298048da664c9fbaabe3ed
Hanming Zhu <hanming@huawen.com>
2020-10-11 20:37:04 +0800

    feat: update Questionnaires to pass in MenuOptions

--- src/routes/questionnaires/Questionnaires.tsx
+++ src/routes/questionnaires/Questionnaires.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import { Grid } from '@material-ui/core';
 import { isBefore, isAfter } from 'date-fns';
+import { useHistory } from 'react-router-dom';
 
 import PageContainer from 'components/pageContainer';
-import { CREATE, QUESTIONNAIRES } from 'constants/routes';
+import { CREATE, EDIT, QUESTIONNAIRES } from 'constants/routes';
 import PaperTabs from 'components/paperTabs';
 import QuestionnaireCard from 'components/questionnaireCard';
 import PageHeader from 'components/pageHeader';
 import ShortButton from 'components/shortButton';
+import { MenuOption } from 'interfaces/components/questionnaireCard';
 
 import { questionnaires } from './mockData';
 
 const Questionnaires: React.FunctionComponent = () => {
+  const history = useHistory();
   const [tabValue, setTabValue] = useState<number>(0);
 
   const breadcrumbs = [{ text: 'Questionnaires', href: QUESTIONNAIRES }];
@@ -38,6 +41,20 @@ const Questionnaires: React.FunctionComponent = () => {
       break;
   }
 
+  const getMenuOptions = (id: number): MenuOption[] => {
+    return [
+      {
+        text: 'Edit',
+        callback: () => history.push(`${QUESTIONNAIRES}/${id}${EDIT}`),
+      },
+      {
+        text: 'Make a copy',
+        // eslint-disable-next-line no-console
+        callback: () => console.log('TODO: Make a copy'),
+      },
+    ];
+  };
+
   return (
     <PageContainer>
       <PageHeader
@@ -50,11 +67,14 @@ const Questionnaires: React.FunctionComponent = () => {
       />
       <PaperTabs value={tabValue} setValue={setTabValue} labels={tabs} />
       <Grid container spacing={3}>
-        {renderedQuestionnaires.map((q) => (
-          <Grid item xs={12} sm={6} lg={4} key={q.name}>
-            <QuestionnaireCard questionnaire={q} />
-          </Grid>
-        ))}
+        {renderedQuestionnaires.map((q) => {
+          const menuOptions = getMenuOptions(q.id);
+          return (
+            <Grid item xs={12} sm={6} lg={4} key={q.name}>
+              <QuestionnaireCard questionnaire={q} menuOptions={menuOptions} />
+            </Grid>
+          );
+        })}
       </Grid>
     </PageContainer>
   );
