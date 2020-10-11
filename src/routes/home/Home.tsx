b765a700e5b3d7389d0d66e0404f6536c6c0188e
Hanming Zhu <hanming@huawen.com>
2020-10-11 11:20:33 +0800

    feat: add breadcrumbs to home

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -3,14 +3,20 @@ import React from 'react';
 import { useUser } from 'contexts/UserContext';
 import { useAuth } from 'contexts/AuthContext';
 import PageContainer from 'components/pageContainer';
+import Breadcrumbs from 'components/breadcrumbs';
+import { HOME } from 'constants/routes';
 
 import './Home.scss';
 
 const Home: React.FunctionComponent = () => {
   const { name } = useUser()!;
   const { logout } = useAuth();
+
+  const breadcrumbs = [{ text: 'Home', href: HOME }];
+
   return (
     <PageContainer>
+      <Breadcrumbs breadcrumbs={breadcrumbs} />
       <div>You&apos;re logged in, {name}!</div>
       <br />
       <button type="button" onClick={logout}>
