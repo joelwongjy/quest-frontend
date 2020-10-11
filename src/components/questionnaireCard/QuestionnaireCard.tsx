5b1719a18fef46a74279461198ec9a04d6d7818b
Hanming Zhu <hanming@huawen.com>
2020-10-11 20:35:51 +0800

    feat: make QuestionnaireCard menu options flexible

--- src/components/questionnaireCard/QuestionnaireCard.tsx
+++ src/components/questionnaireCard/QuestionnaireCard.tsx
@@ -11,19 +11,26 @@ import {
 } from '@material-ui/core';
 import { format, formatDistanceToNow } from 'date-fns';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Link } from 'react-router-dom';
 
 import { QuestComponentProps } from 'interfaces/components/common';
 import Card from 'components/card';
-import { QuestionnaireCardInfo } from 'interfaces/components/questionnaireCard';
+import {
+  MenuOption,
+  QuestionnaireCardInfo,
+} from 'interfaces/components/questionnaireCard';
+import { QUESTIONNAIRES, RESPONSES } from 'constants/routes';
 
 import { useStyles } from './questionnaireCard.styles';
 
 interface QuestionnaireCardProps extends QuestComponentProps {
   questionnaire: QuestionnaireCardInfo;
+  menuOptions?: MenuOption[];
 }
 
 const QuestionnaireCard: React.FunctionComponent<QuestionnaireCardProps> = ({
   questionnaire,
+  menuOptions = null,
 }) => {
   const classes = useStyles();
   const [anchorEle, setAnchorEle] = useState<null | HTMLElement>(null);
@@ -70,26 +77,37 @@ const QuestionnaireCard: React.FunctionComponent<QuestionnaireCardProps> = ({
             </>
           }
           action={
-            <>
-              <IconButton
-                aria-label="more options"
-                aria-controls="more options"
-                aria-haspopup="true"
-                onClick={handleClick}
-              >
-                <MoreVertIcon />
-              </IconButton>
-              <Menu
-                id={`questionnaire-menu-${questionnaire.id}`}
-                anchorEl={anchorEle}
-                keepMounted
-                open={Boolean(anchorEle)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Edit</MenuItem>
-                <MenuItem onClick={handleClose}>Make a copy</MenuItem>
-              </Menu>
-            </>
+            menuOptions && (
+              <>
+                <IconButton
+                  aria-label="more options"
+                  aria-controls="more options"
+                  aria-haspopup="true"
+                  onClick={handleClick}
+                >
+                  <MoreVertIcon />
+                </IconButton>
+                <Menu
+                  id={`questionnaire-menu-${questionnaire.id}`}
+                  anchorEl={anchorEle}
+                  keepMounted
+                  open={Boolean(anchorEle)}
+                  onClose={handleClose}
+                >
+                  {menuOptions.map((m) => (
+                    <MenuItem
+                      onClick={() => {
+                        handleClose();
+                        m.callback();
+                      }}
+                      key={`${m.text}-${questionnaire.id}`}
+                    >
+                      {m.text}
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>
+            )
           }
         />
         <CardContent>
@@ -111,7 +129,12 @@ const QuestionnaireCard: React.FunctionComponent<QuestionnaireCardProps> = ({
           </Typography>
         </CardContent>
         <CardActions className={classes.actions}>
-          <Button size="small" className={classes.button}>
+          <Button
+            size="small"
+            className={classes.button}
+            component={Link}
+            to={`${QUESTIONNAIRES}/${questionnaire.id}${RESPONSES}`}
+          >
             View Responses
           </Button>
         </CardActions>
