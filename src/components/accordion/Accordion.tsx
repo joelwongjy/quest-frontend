11ec7cef42d56e795b79a5b4bb4cb3ba75730389
Hanming Zhu <hanming@huawen.com>
2020-10-11 22:28:48 +0800

    feat: build accordion component

---
+++ src/components/accordion/Accordion.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionProps,
+  AccordionSummary,
+  Typography,
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import { useStyles } from './accordion.styles';
+
+interface QuestAccordionProps {
+  heading: string;
+  defaultExpanded?: boolean;
+}
+
+const QuestAccordion: React.FunctionComponent<
+  QuestAccordionProps & AccordionProps
+> = ({ heading, defaultExpanded = true, children, ...props }) => {
+  const classes = useStyles();
+  return (
+    <Accordion
+      className={classes.root}
+      defaultExpanded={defaultExpanded}
+      {...props}
+    >
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel-content"
+        id={`panel-header-${heading}`}
+      >
+        <Typography className={classes.heading}>{heading}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>{children}</AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default QuestAccordion;
