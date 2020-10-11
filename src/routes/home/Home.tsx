f2024f057fd19b9ccbebaa72fc04eb0567ed0bc6
Hanming Zhu <hanming@huawen.com>
2020-10-11 09:22:45 +0800

    feat: update home and login to use the latest pageContainer

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { useUser } from 'contexts/UserContext';
 import { useAuth } from 'contexts/AuthContext';
+import PageContainer from 'components/pageContainer';
 
 import './Home.scss';
 
@@ -9,13 +10,13 @@ const Home: React.FunctionComponent = () => {
   const { name } = useUser()!;
   const { logout } = useAuth();
   return (
-    <div style={{ padding: '1rem' }}>
+    <PageContainer>
       <div>You&apos;re logged in, {name}!</div>
       <br />
       <button type="button" onClick={logout}>
         Log out
       </button>
-    </div>
+    </PageContainer>
   );
 };
 
--- src/routes/login/Login.tsx
+++ src/routes/login/Login.tsx
@@ -36,69 +36,71 @@ const Login: React.FunctionComponent = () => {
   };
 
   return (
-    <PageContainer>
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
-          <form className={classes.form} onSubmit={handleLogin}>
-            <Input
-              required
-              id="username"
-              label="Username"
-              name="username"
-              autoComplete="on"
-              autoFocus
-              value={loginUsername}
-              onChange={(e) => {
-                clearError();
-                setLoginUsername(e.target.value);
-              }}
-            />
-            <Input
-              required
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="on"
-              value={loginPassword}
-              onChange={(e) => {
-                clearError();
-                setLoginPassword(e.target.value);
-              }}
-            />
-            {/* <FormControlLabel
+    <PageContainer hasDrawer={false} hasContentPadding={false}>
+      <Grid container component="main" className={classes.root}>
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Login
+            </Typography>
+            <form className={classes.form} onSubmit={handleLogin}>
+              <Input
+                required
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="on"
+                autoFocus
+                value={loginUsername}
+                onChange={(e) => {
+                  clearError();
+                  setLoginUsername(e.target.value);
+                }}
+              />
+              <Input
+                required
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="on"
+                value={loginPassword}
+                onChange={(e) => {
+                  clearError();
+                  setLoginPassword(e.target.value);
+                }}
+              />
+              {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember Me"
             /> */}
-            <LongButton type="submit" value="Submit">
-              Sign In
-            </LongButton>
-            <Grid container>
-              <Grid item xs>
-                <Link href="routes/login" variant="body2" color="secondary">
-                  Forgot password?
-                </Link>
+              <LongButton type="submit" value="Submit">
+                Sign In
+              </LongButton>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="routes/login" variant="body2" color="secondary">
+                    Forgot password?
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
-          </form>
-          <div
-            style={{
-              color: 'red',
-              marginTop: '0.5rem',
-              visibility: isError ? 'visible' : 'hidden',
-              height: '2rem',
-            }}
-          >
-            {loginErrMsg}
+            </form>
+            <div
+              style={{
+                color: 'red',
+                marginTop: '0.5rem',
+                visibility: isError ? 'visible' : 'hidden',
+                height: '2rem',
+              }}
+            >
+              {loginErrMsg}
+            </div>
           </div>
-        </div>
+        </Grid>
       </Grid>
     </PageContainer>
   );
