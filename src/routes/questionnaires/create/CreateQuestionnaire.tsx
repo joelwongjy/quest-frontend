ecbc6c77fcf727e182fff1d8aaac8110f733f7b6
Hanming Zhu <hanming@huawen.com>
2020-10-15 11:09:51 +0800

    refactor: update createQuestionnaire view, but still WIP

--- src/routes/questionnaires/create/CreateQuestionnaire.tsx
+++ src/routes/questionnaires/create/CreateQuestionnaire.tsx
@@ -1,78 +1,83 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from 'react';
-
+import { Grid, Paper } from '@material-ui/core';
 import nextId from 'react-id-generator';
+import { useSelector, useDispatch } from 'react-redux';
 
 import PageContainer from 'components/pageContainer';
 import { CREATE, QUESTIONNAIRES } from 'constants/routes';
 import PageHeader from 'components/pageHeader';
 import Accordion from 'components/accordion';
-import QuestDatePicker from 'components/questDatePicker';
-import QuestProgram, { QuestClass, Question } from 'interfaces/models/admin';
-import ProgramClassPicker from 'components/programClassPicker';
-import { Grid, Paper } from '@material-ui/core';
 import QuestionCard from 'components/questionCard';
 import ShortButton from 'components/shortButton';
 import LongButton from 'components/longButton';
-import { programs } from '../mockData';
+import { useUser } from 'contexts/UserContext';
+import { RootState } from 'reducers/rootReducer';
+import {
+  QuestionnaireDux,
+  setPostEndTime,
+  setPostStartTime,
+  setPreEndTime,
+  setPreStartTime,
+} from 'reducers/questionnaireDux';
+
+import DateAccordion from '../dateAccordion';
+import AssignAccordion from '../assignAccordion';
 
 const CreateQuestionnaire: React.FunctionComponent = () => {
+  const user = useUser();
+  const [programmeId, setProgrammeId] = useState<number>(1);
+  const [classId, setClassId] = useState<number>(1);
+
+  const dispatch = useDispatch();
+  const selectQuestionnaire = (state: RootState): QuestionnaireDux =>
+    state.questionnaire;
+
+  const questionnaire = useSelector(selectQuestionnaire);
+  const { type, questionWindows } = questionnaire;
+
   const breadcrumbs = [
     { text: 'Questionnaires', href: QUESTIONNAIRES },
     { text: 'Create New', href: `${QUESTIONNAIRES}/${CREATE}` },
   ];
 
-  const [startDate, setStartDate] = useState<Date>(new Date());
-  const [endDate, setEndDate] = useState<Date>(new Date());
-  const [questPrograms, setQuestProgrames] = useState<QuestProgram[]>(programs);
-  const [questClasses, setQuestClasses] = useState<QuestClass[]>([]);
-  const [questProgramId, setQuestProgramId] = useState<number>(1);
-  const [questClassId, setQuestClassId] = useState<number>(1);
-  const [createdSharedQuestions, setCreatedSharedQuestions] = useState<
-    Question[]
-  >([]);
-  const [createdSpecialQuestions, setCreatedSpecialQuestions] = useState<
-    Question[]
-  >([]);
-
-  const startDateCallback = (start: Date) => {
-    setStartDate(start);
-  };
-
-  const endDateCallback = (end: Date) => {
-    setEndDate(end);
-  };
-
-  const programCallback = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setQuestProgramId(event.target.value as number);
+  const programmeCallback = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setProgrammeId(event.target.value as number);
   };
 
   const questClassCallback = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setQuestClassId(event.target.value as number);
+    setClassId(event.target.value as number);
   };
 
   return (
     <PageContainer>
       <PageHeader breadcrumbs={breadcrumbs} />
       <Paper elevation={0} style={{ background: 'white' }}>
-        <Accordion heading="Step 1: Set the duration">
-          <QuestDatePicker
-            startDate={startDate}
-            endDate={endDate}
-            startDateCallback={startDateCallback}
-            endDateCallback={endDateCallback}
-          />
-        </Accordion>
-        <Accordion heading="Step 2: Assign the questionnaire">
-          <ProgramClassPicker
-            programs={questPrograms}
-            selectedQuestProgramId={questProgramId}
-            selectedQuestClassId={questClassId}
-            programCallback={programCallback}
-            questClassCallback={questClassCallback}
-          />
-        </Accordion>
-        <Accordion heading="Step 3: Create the shared questions">
+        <DateAccordion
+          type={type}
+          preStartDate={questionWindows[0].startAt}
+          preStartDateCallback={(date: Date) => dispatch(setPreStartTime(date))}
+          preEndDate={questionWindows[0].endAt}
+          preEndDateCallback={(date: Date) => dispatch(setPreEndTime(date))}
+          postStartDate={
+            questionWindows.length > 1 ? questionWindows[1].startAt : undefined
+          }
+          postStartDateCallback={(date: Date) =>
+            dispatch(setPostStartTime(date))
+          }
+          postEndDate={
+            questionWindows.length > 1 ? questionWindows[1].startAt : undefined
+          }
+          postEndDateCallback={(date: Date) => dispatch(setPostEndTime(date))}
+        />
+        <AssignAccordion
+          user={user!}
+          programmeId={programmeId}
+          programmeCallback={programmeCallback}
+          classId={classId}
+          classCallback={questClassCallback}
+        />
+        {/* <Accordion heading="Step 3: Create the shared questions">
           <Grid container spacing={3}>
             <Grid item xs={12} alignItems="flex-end">
               <ShortButton
@@ -109,45 +114,8 @@ const CreateQuestionnaire: React.FunctionComponent = () => {
               );
             })}
           </Grid>
-        </Accordion>
-        <Accordion heading="Step 4: Create the special questions">
-          <Grid container spacing={3}>
-            <Grid item xs={12} alignItems="flex-end">
-              <ShortButton
-                onClick={() => {
-                  const createdSpecialQuestionsCopy = createdSpecialQuestions.slice();
-                  createdSpecialQuestionsCopy.push({
-                    id: nextId(),
-                    questionText: '',
-                    questionType: 'MCQ',
-                    options: [],
-                  });
-                  setCreatedSpecialQuestions(createdSpecialQuestionsCopy);
-                }}
-              >
-                New Question
-              </ShortButton>
-            </Grid>
-            {createdSpecialQuestions.map((q: Question, index: number) => {
-              return (
-                <Grid item xs={12} key={q.id}>
-                  <QuestionCard
-                    question={q}
-                    questionIndex={index}
-                    mode="new"
-                    handleDelete={() => {
-                      let createdSpecialQuestionsCopy = createdSpecialQuestions.slice();
-                      createdSpecialQuestionsCopy = createdSpecialQuestionsCopy.filter(
-                        (question) => question.id !== q.id
-                      );
-                      setCreatedSpecialQuestions(createdSpecialQuestionsCopy);
-                    }}
-                  />
-                </Grid>
-              );
-            })}
-          </Grid>
-        </Accordion>
+        </Accordion> */}
+
         <Grid container justify="flex-end">
           <LongButton>Finish</LongButton>
         </Grid>
