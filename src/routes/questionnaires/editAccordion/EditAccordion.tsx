6059b3deba66ddc84f3e73f31f02eb3081f9fa15
Hanming Zhu <hanming@huawen.com>
2020-10-15 11:09:14 +0800

    feat: build editAccordion

---
+++ src/routes/questionnaires/editAccordion/EditAccordion.tsx
@@ -0,0 +1,12 @@
+import { QuestionnairePostData } from 'interfaces/api/questionnaires';
+import React from 'react';
+
+interface EditAccordion {
+  questionnaire: QuestionnairePostData;
+}
+
+const EditAccordion: React.FC<EditAccordion> = ({ questionnaire }) => {
+  return <></>;
+};
+
+export default EditAccordion;
