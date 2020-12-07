79c627a74e8e9d468424f754a4a9c78f7bbf0850
Asthenosphere <e0271247@u.nus.edu>
2020-12-07 12:01:32 +0800

    Build dialog component

--- src/componentWrappers/questDialog/QuestDialog.tsx
+++ src/componentWrappers/questDialog/QuestDialog.tsx
@@ -1,3 +1,11 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@material-ui/core';
 import { QuestComponentProps } from 'interfaces/components/common';
 import React from 'react';
 
@@ -7,19 +15,50 @@ interface QuestDialogProps extends QuestComponentProps {
   isDialogOpen: boolean;
   dialogHeader: string;
   dialogContent: string;
-  confirmHandler?: () => void;
-  cancelHandler?: () => void;
+  hasConfirm: boolean;
+  closeHandler: () => void;
+  confirmHandler: () => void;
+  cancelHandler: () => void;
 }
 
 const QuestDialog: React.FunctionComponent<QuestDialogProps> = ({
-  theme,
   isDialogOpen,
   dialogHeader,
   dialogContent,
+  hasConfirm,
+  closeHandler,
   confirmHandler,
   cancelHandler,
 }) => {
-  return <></>;
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Dialog
+        open={isDialogOpen}
+        onClose={closeHandler}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{dialogHeader}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-content">
+            {dialogContent}
+          </DialogContentText>
+        </DialogContent>
+        {hasConfirm ? (
+          <DialogActions>
+            <Button onClick={cancelHandler}>Cancel</Button>
+            <Button onClick={confirmHandler}>Confirm</Button>
+          </DialogActions>
+        ) : (
+          <DialogActions>
+            <Button onClick={cancelHandler}>Okay</Button>
+          </DialogActions>
+        )}
+      </Dialog>
+    </div>
+  );
 };
 
 export default QuestDialog;
