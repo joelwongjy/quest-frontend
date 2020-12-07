1330542cdb53ce8603f114f66f596e8a1c1c7ada
Hanming Zhu <hanming@huawen.com>
2020-12-07 13:27:52 +0800

    feat: add new context for error to prevent insane nesting

--- src/contexts/AppProviders.tsx
+++ src/contexts/AppProviders.tsx
@@ -6,13 +6,16 @@ import DateFnsUtils from '@material-ui/pickers/adapter/date-fns';
 import { theme } from 'styles/theme';
 import { AuthProvider } from './AuthContext';
 import { UserProvider } from './UserContext';
+import { ErrorProvider } from './ErrorContext';
 
 const AppProviders: React.FunctionComponent = ({ children }) => {
   return (
     <MuiThemeProvider theme={theme}>
       <LocalizationProvider dateAdapter={DateFnsUtils}>
         <AuthProvider>
-          <UserProvider>{children}</UserProvider>
+          <UserProvider>
+            <ErrorProvider>{children}</ErrorProvider>
+          </UserProvider>
         </AuthProvider>
       </LocalizationProvider>
     </MuiThemeProvider>
