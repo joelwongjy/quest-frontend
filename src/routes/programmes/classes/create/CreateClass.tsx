de8762ebafe7111f08b67dd207faa1cd0ba158dc
Asthenosphere <e0271247@u.nus.edu>
2020-12-16 10:38:58 +0800

    Fix bug in breadcumbs routing

--- src/routes/programmes/classes/create/CreateClass.tsx
+++ src/routes/programmes/classes/create/CreateClass.tsx
@@ -1,42 +1,35 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { Dispatch, useEffect, useReducer, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Grid, Paper, Typography } from '@material-ui/core';
-import SingleIcon from '@material-ui/icons/DescriptionOutlined';
-import PostIcon from '@material-ui/icons/Description';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useReducer } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 
 import ApiService from 'services/apiService';
 import ClassForm from 'components/classForm';
 import PageContainer from 'components/pageContainer';
 import PageHeader from 'components/pageHeader';
-import ProgrammeForm from 'components/programmeForm';
-import QuestAlert from 'componentWrappers/questAlert';
-import QuestButton from 'componentWrappers/questButton';
 import { PROGRAMMES, CLASSES, CREATE } from 'constants/routes';
 import { ClassMode } from 'interfaces/models/classes';
 import { ProgrammeListData } from 'interfaces/models/programmes';
-import { useUser } from 'contexts/UserContext';
-import { useError } from 'contexts/ErrorContext';
-import { RouteState } from 'interfaces/routes/common';
+import { RouteParams, RouteState } from 'interfaces/routes/common';
 import { programmes } from 'routes/questionnaires/mockData';
-
-import { useStyles } from './createClass.styles';
 
 interface CreateClassState extends RouteState {
   programme: ProgrammeListData;
 }
 
 const CreateClass: React.FunctionComponent = () => {
-  const user = useUser();
-  const classes = useStyles();
   const history = useHistory();
-  const { setHasError } = useError();
+  const { id } = useRouteMatch<RouteParams>({
+    path: `${PROGRAMMES}/:id${CLASSES}${CREATE}`,
+  })!.params;
+  const programmeId = parseInt(id, 10);
 
   const breadcrumbs = [
     { text: 'Programmes', href: PROGRAMMES },
-    { text: 'Classes', href: `${PROGRAMMES}/:id${CLASSES}` },
-    { text: 'Create', href: `${PROGRAMMES}/:id${CLASSES}/${CREATE}` },
+    { text: 'Classes', href: `${PROGRAMMES}/${programmeId}${CLASSES}` },
+    {
+      text: 'Create',
+      href: `${PROGRAMMES}/${programmeId}${CLASSES}/${CREATE}`,
+    },
   ];
 
   const [state, setState] = useReducer(
@@ -143,15 +136,6 @@ const CreateClass: React.FunctionComponent = () => {
         cancelCallback={() =>
           history.push(`${PROGRAMMES}/${state.programme.id}${CLASSES}`)
         }
-      />
-      <QuestAlert
-        isAlertOpen={state.isAlertOpen!}
-        hasConfirm={state.hasConfirm!}
-        alertHeader={state.alertHeader!}
-        alertMessage={state.alertMessage!}
-        closeHandler={state.closeHandler!}
-        confirmHandler={state.confirmHandler}
-        cancelHandler={state.cancelHandler}
       />
     </PageContainer>
   );
