30989a01de8c3b3ab2e242d89ef6c99397b5e28e
Asthenosphere <e0271247@u.nus.edu>
2020-12-19 13:32:35 +0800

    View card for short answer question

--- src/components/shortAnswerQuestion/view/ViewShortAnswerQuestion.tsx
+++ src/components/shortAnswerQuestion/view/ViewShortAnswerQuestion.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormControl, FormGroup } from '@material-ui/core';
+import { FormControl, FormGroup, Grid, Typography } from '@material-ui/core';
 
 import QuestTextField from 'componentWrappers/questTextField';
 
@@ -47,6 +47,94 @@ const ViewShortAnswerQuestion: React.FunctionComponent<ViewShortAnswerQuestionPr
         </div>
       );
     }
+
+    if (answerBefore !== undefined || answerAfter !== undefined) {
+      return (
+        <div className={classes.top}>
+          <div className={classes.textfieldContainer}>
+            <FormControl style={{ width: '100%' }}>
+              <QuestTextField
+                className={classes.textfield}
+                label="Question"
+                value={
+                  answerBefore
+                    ? answerBefore.questionOrder.questionText
+                    : answerAfter!.questionOrder.questionText
+                }
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+            </FormControl>
+          </div>
+          <Grid container justify="space-between">
+            <Grid
+              item
+              xs={6}
+              style={{
+                paddingLeft: '0.5rem',
+                paddingRight: '1.5rem',
+                borderRight: '2px solid grey',
+              }}
+            >
+              <Grid container justify="center">
+                <Typography variant="h6" style={{ color: '#695F5F' }}>
+                  Before
+                </Typography>
+              </Grid>
+              {answerBefore !== undefined ? (
+                <QuestTextField
+                  placeholder="Long Answer"
+                  label="Answer"
+                  value={answerBefore.textResponse}
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                  style={{ marginTop: '1rem' }}
+                />
+              ) : (
+                <Typography>
+                  This question was added after the student attempted the
+                  pre-programme questionnaire.
+                </Typography>
+              )}
+            </Grid>
+            <Grid
+              item
+              xs={6}
+              style={{
+                paddingLeft: '0.5rem',
+                paddingRight: '1.5rem',
+                borderRight: '2px solid grey',
+              }}
+            >
+              <Grid container justify="center">
+                <Typography variant="h6" style={{ color: '#695F5F' }}>
+                  Before
+                </Typography>
+              </Grid>
+              {answerAfter !== undefined ? (
+                <QuestTextField
+                  placeholder="Long Answer"
+                  label="Answer"
+                  value={answerAfter.textResponse}
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                  style={{ marginTop: '1rem' }}
+                />
+              ) : (
+                <Typography>
+                  This question has been modified or deleted after the student
+                  attempted the pre-programme questionnaire.
+                </Typography>
+              )}
+            </Grid>
+          </Grid>
+        </div>
+      );
+    }
+
     return <></>;
   };
 
