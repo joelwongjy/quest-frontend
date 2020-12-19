2ee8b29595feac9301406c0cbb56938035ad1c9f
Asthenosphere <e0271247@u.nus.edu>
2020-12-19 11:46:36 +0800

    Add handling of modification of mcq

--- src/components/mcqQuestion/view/ViewMcqQuestion.tsx
+++ src/components/mcqQuestion/view/ViewMcqQuestion.tsx
@@ -151,6 +151,70 @@ const ViewMcqQuestion: React.FunctionComponent<ViewMcqQuestionProps> = ({
           </div>
         );
       }
+    } else if (answerBefore !== undefined) {
+      return (
+        <div className={classes.top}>
+          <div className={classes.textfieldContainer}>
+            <FormControl style={{ width: '100%' }}>
+              <QuestTextField
+                className={classes.textfield}
+                label="Question"
+                value={answerBefore.questionOrder.questionText}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+            </FormControl>
+          </div>
+          <Grid container justify="space-between">
+            <Grid
+              item
+              xs={6}
+              style={{ paddingLeft: '0.5rem', borderRight: '2px solid grey' }}
+            >
+              <Grid container justify="center">
+                <Typography variant="h6" style={{ color: '#695F5F' }}>
+                  Before
+                </Typography>
+              </Grid>
+              <RadioGroup>
+                {answerBefore.questionOrder.options.map((option, index) => (
+                  <div
+                    // eslint-disable-next-line react/no-array-index-key
+                    key={`option-${answerBefore.answerId}-${index}`}
+                    style={{
+                      width: '100%',
+                      display: 'flex',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <FormControlLabel
+                      checked={
+                        option.optionText === answerBefore.option?.optionText
+                      }
+                      value={option.optionText}
+                      style={{ width: '100%' }}
+                      control={<Radio />}
+                      label={option.optionText}
+                    />
+                  </div>
+                ))}
+              </RadioGroup>
+            </Grid>
+            <Grid item xs={6} style={{ paddingLeft: '1.5rem' }}>
+              <Grid container justify="center">
+                <Typography variant="h6" style={{ color: '#695F5F' }}>
+                  After
+                </Typography>
+              </Grid>
+              <Typography>
+                This question has been modified or deleted after the student
+                attempted the pre-programme questionnaire.
+              </Typography>
+            </Grid>
+          </Grid>
+        </div>
+      );
     }
     return <></>;
   };
