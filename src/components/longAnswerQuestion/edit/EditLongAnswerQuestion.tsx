9b5bc71884caf5ce1e4c9b853fd91e6d06e96c0e
Hanming Zhu <hanming@huawen.com>
2020-12-21 21:26:19 +0800

    refactor: strip out unneeded logic for long answer

--- src/components/longAnswerQuestion/edit/EditLongAnswerQuestion.tsx
+++ src/components/longAnswerQuestion/edit/EditLongAnswerQuestion.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import {
-  FormControl,
-  FormGroup,
-  FormHelperText,
-  FormLabel,
-} from '@material-ui/core';
+import { FormControl, FormGroup, FormHelperText } from '@material-ui/core';
 
-import { QuestionMode } from 'interfaces/models/questionnaires';
 import QuestTextField from 'componentWrappers/questTextField';
 import { useError } from 'contexts/ErrorContext';
 import { QuestionnaireDuxQuestion } from 'reducers/questionnaireDux';
@@ -15,14 +9,12 @@ import { useStyles } from './editLongAnswerQuestion.styles';
 
 interface EditLongAnswerQuestionProps {
   question: QuestionnaireDuxQuestion;
-  mode: QuestionMode;
   updateQuestion: (newQuestion: QuestionnaireDuxQuestion) => void;
   dropdown: React.ReactNode;
 }
 
 const EditLongAnswerQuestion: React.FunctionComponent<EditLongAnswerQuestionProps> = ({
   question,
-  mode,
   updateQuestion,
   dropdown,
 }) => {
@@ -36,49 +28,34 @@ const EditLongAnswerQuestion: React.FunctionComponent<EditLongAnswerQuestionProp
 
   const hasQuestionTextError = hasError && question.questionText === '';
 
-  const renderQuestion = () => {
-    switch (mode) {
-      case QuestionMode.EDIT || QuestionMode.NEW:
-        return (
-          <div className={classes.top}>
-            <div className={classes.textfieldContainer}>
-              <FormControl
-                style={{ width: '100%' }}
-                error={hasQuestionTextError}
-              >
-                <QuestTextField
-                  required
-                  className={classes.textfield}
-                  label="Question"
-                  variant="filled"
-                  value={question.questionText}
-                  onChange={(e) => updateText(e.target.value)}
-                />
-                {hasQuestionTextError && (
-                  <FormHelperText>The question cannot be blank!</FormHelperText>
-                )}
-              </FormControl>
-              {dropdown}
-            </div>
+  return (
+    <FormGroup className={classes.card}>
+      <div className={classes.top}>
+        <div className={classes.textfieldContainer}>
+          <FormControl style={{ width: '100%' }} error={hasQuestionTextError}>
             <QuestTextField
-              disabled
-              id="disabled"
-              defaultValue="Long Answer"
-              rows={3}
+              required
+              className={classes.textfield}
+              label="Question"
+              variant="filled"
+              value={question.questionText}
+              onChange={(e) => updateText(e.target.value)}
             />
-          </div>
-        );
-      default:
-        return (
-          <div className={classes.top}>
-            <FormLabel component="legend">{question}</FormLabel>
-            <QuestTextField placeholder="Long Answer" />
-          </div>
-        );
-    }
-  };
-
-  return <FormGroup className={classes.card}>{renderQuestion()}</FormGroup>;
+            {hasQuestionTextError && (
+              <FormHelperText>The question cannot be blank!</FormHelperText>
+            )}
+          </FormControl>
+          {dropdown}
+        </div>
+        <QuestTextField
+          disabled
+          id="disabled"
+          defaultValue="Long Answer"
+          rows={3}
+        />
+      </div>
+    </FormGroup>
+  );
 };
 
 export default EditLongAnswerQuestion;
