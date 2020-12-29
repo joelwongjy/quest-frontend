4fb61000bff43c27627a0581475fdb4452b473b3
Hanming Zhu <hanming@huawen.com>
2020-12-29 14:29:21 +0800

    fix: update text field to be secondary by default

--- src/componentWrappers/questTextField/QuestTextField.tsx
+++ src/componentWrappers/questTextField/QuestTextField.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { TextField, TextFieldProps } from '@material-ui/core';
 
 const QuestTextField: React.FunctionComponent<TextFieldProps> = (props) => {
-  return <TextField color="primary" variant="outlined" fullWidth {...props} />;
+  return (
+    <TextField color="secondary" variant="outlined" fullWidth {...props} />
+  );
 };
 
 export default QuestTextField;
