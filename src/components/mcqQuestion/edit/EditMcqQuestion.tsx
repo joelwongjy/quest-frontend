36c091a27c363de7f964c430b7b0d07c6bb08c86
Hanming Zhu <hanming@huawen.com>
2020-12-29 14:25:46 +0800

    refactor: clean up question edit components

--- src/components/mcqQuestion/edit/EditMcqQuestion.tsx
+++ src/components/mcqQuestion/edit/EditMcqQuestion.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  FormGroup,
-  IconButton,
-  FormControl,
-  FormHelperText,
-} from '@material-ui/core';
+import { IconButton, FormControl, FormHelperText } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 import QuestButton from 'componentWrappers/questButton';
@@ -17,7 +12,6 @@ import { useStyles } from './editMcqQuestion.styles';
 interface EditMcqQuestionProps {
   question: QuestionnaireDuxQuestion;
   updateQuestion: (newQuestion: QuestionnaireDuxQuestion) => void;
-  dropdown: React.ReactNode;
   alertCallback: (
     isAlertOpen: boolean,
     hasConfirm: boolean,
@@ -31,7 +25,6 @@ interface EditMcqQuestionProps {
 const EditMcqQuestion: React.FunctionComponent<EditMcqQuestionProps> = ({
   question,
   updateQuestion,
-  dropdown,
   alertCallback,
 }) => {
   const classes = useStyles();
@@ -78,71 +71,45 @@ const EditMcqQuestion: React.FunctionComponent<EditMcqQuestionProps> = ({
     updateQuestion({ ...question, options: newOptions });
   };
 
-  const updateText = (newText: string) => {
-    const newQuestion = { ...question, questionText: newText };
-    updateQuestion(newQuestion);
-  };
-
-  const hasQuestionTextError = hasError && question.questionText === '';
   const hasOptionError =
     hasError &&
     question.options.filter((q) => q.optionText !== '').length === 0;
 
   return (
-    <FormGroup className={classes.card}>
-      <div className={classes.top}>
-        <div className={classes.textfieldContainer}>
-          <FormControl style={{ width: '100%' }} error={hasQuestionTextError}>
+    <div className={classes.top}>
+      {question.options.map((option, index) => (
+        <div
+          // eslint-disable-next-line react/no-array-index-key
+          key={`option-${question.duxId}-${index}`}
+          className={classes.optionContainer}
+        >
+          <FormControl style={{ width: '100%' }} error={hasOptionError}>
             <QuestTextField
               required
-              className={classes.textfield}
-              label="Question"
-              variant="filled"
-              value={question.questionText}
-              onChange={(e) => updateText(e.target.value)}
+              placeholder={`Option ${index + 1}`}
+              value={option.optionText}
+              onChange={(e) => updateOption(e.target.value, index)}
+              className={classes.option}
             />
-            {hasQuestionTextError && (
-              <FormHelperText>The question cannot be blank!</FormHelperText>
+            {hasOptionError && index === 0 && (
+              <FormHelperText>At least one option is required!</FormHelperText>
             )}
           </FormControl>
-          {dropdown}
+          {(question.options.length > 1 || index !== 0) && (
+            <IconButton
+              aria-label="delete"
+              onClick={() => deleteOption(index)}
+              style={{ marginLeft: '0.5rem', marginTop: '1rem' }}
+            >
+              <DeleteIcon />
+            </IconButton>
+          )}
         </div>
-        {question.options.map((option, index) => (
-          <div
-            // eslint-disable-next-line react/no-array-index-key
-            key={`option-${question.duxId}-${index}`}
-            style={{ width: '100%', display: 'flex', alignItems: 'center' }}
-          >
-            <FormControl style={{ width: '100%' }} error={hasOptionError}>
-              <QuestTextField
-                required
-                placeholder={`Option ${index + 1}`}
-                value={option.optionText}
-                onChange={(e) => updateOption(e.target.value, index)}
-                className={classes.option}
-              />
-              {hasOptionError && index === 0 && (
-                <FormHelperText>
-                  At least one option is required!
-                </FormHelperText>
-              )}
-            </FormControl>
-            {(question.options.length > 1 || index !== 0) && (
-              <IconButton
-                aria-label="delete"
-                onClick={() => deleteOption(index)}
-                style={{ marginLeft: '0.5rem' }}
-              >
-                <DeleteIcon />
-              </IconButton>
-            )}
-          </div>
-        ))}
-        <QuestButton onClick={addOption} className={classes.optionButton}>
-          Add Option
-        </QuestButton>
-      </div>
-    </FormGroup>
+      ))}
+      <QuestButton onClick={addOption} className={classes.optionButton}>
+        Add Option
+      </QuestButton>
+    </div>
   );
 };
 
--- src/components/scaleQuestion/edit/EditScaleQuestion.tsx
+++ src/components/scaleQuestion/edit/EditScaleQuestion.tsx
@@ -1,74 +1,28 @@
 import React from 'react';
-import {
-  FormControl,
-  FormGroup,
-  FormHelperText,
-  Grid,
-} from '@material-ui/core';
-
-import QuestTextField from 'componentWrappers/questTextField';
-import { useError } from 'contexts/ErrorContext';
-import { QuestionnaireDuxQuestion } from 'reducers/questionnaireDux';
+import { Grid } from '@material-ui/core';
 
 import QuestSlider from 'componentWrappers/questSlider';
+
 import { useStyles } from './editScaleQuestion.styles';
 
-interface EditScaleQuestionProps {
-  question: QuestionnaireDuxQuestion;
-  updateQuestion: (newQuestion: QuestionnaireDuxQuestion) => void;
-  dropdown: React.ReactNode;
-}
-
-const EditScaleQuestion: React.FunctionComponent<EditScaleQuestionProps> = ({
-  question,
-  updateQuestion,
-  dropdown,
-}) => {
+const EditScaleQuestion: React.FC = () => {
   const classes = useStyles();
-  const { hasError } = useError();
-
-  const updateText = (newText: string) => {
-    const newQuestion = { ...question, questionText: newText };
-    updateQuestion(newQuestion);
-  };
-
-  const hasQuestionTextError = hasError && question.questionText === '';
 
   return (
-    <FormGroup className={classes.card}>
-      <div className={classes.top}>
-        <div className={classes.textfieldContainer}>
-          <FormControl style={{ width: '100%' }} error={hasQuestionTextError}>
-            <QuestTextField
-              required
-              className={classes.textfield}
-              label="Question"
-              variant="filled"
-              value={question.questionText}
-              onChange={(e) => updateText(e.target.value)}
-            />
-            {hasQuestionTextError && (
-              <FormHelperText>The question cannot be blank!</FormHelperText>
-            )}
-          </FormControl>
-          {dropdown}
-        </div>
-        <Grid container alignItems="center" justify="space-around">
-          <QuestSlider
-            defaultValue={3}
-            aria-labelledby="discrete-slider"
-            valueLabelDisplay="on"
-            step={1}
-            marks
-            min={1}
-            max={5}
-            value={3}
-            className={classes.scale}
-            disabled
-          />
-        </Grid>
-      </div>
-    </FormGroup>
+    <Grid container alignItems="center" justify="space-around">
+      <QuestSlider
+        defaultValue={3}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="on"
+        step={1}
+        marks
+        min={1}
+        max={5}
+        value={3}
+        className={classes.scale}
+        disabled
+      />
+    </Grid>
   );
 };
 
