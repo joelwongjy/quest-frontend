722584be2620427e202d43336f9df98cb6049c03
Hanming Zhu <hanming@huawen.com>
2021-01-04 00:00:27 +0800

    feat: style scale question response view

--- src/components/scaleQuestion/view/ViewScaleQuestion.tsx
+++ src/components/scaleQuestion/view/ViewScaleQuestion.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { FormGroup, Grid, Typography } from '@material-ui/core';
+import { FormGroup, Grid } from '@material-ui/core';
 
 import { AnswerData } from 'interfaces/models/answers';
 import QuestSlider from 'componentWrappers/questSlider';
+import { Scale } from 'interfaces/models/questions';
+
 import { useStyles } from './viewScaleQuestion.styles';
 
 interface ViewScaleQuestionProps {
@@ -18,91 +20,66 @@ const ViewScaleQuestion: React.FunctionComponent<ViewScaleQuestionProps> = ({
 }) => {
   const classes = useStyles();
 
+  const scaleToNumberMap: { [key: string]: number } = {
+    [Scale.ONE]: 1,
+    [Scale.TWO]: 2,
+    [Scale.THREE]: 3,
+    [Scale.FOUR]: 4,
+    [Scale.FIVE]: 5,
+  };
+
   const renderQuestion = () => {
     if (answer) {
+      const value = scaleToNumberMap[answer.option!.optionText!];
+      const answerClass = `is-blue-${value}`;
       return (
         <div className={classes.top}>
-          <Grid container alignItems="center" justify="space-around">
-            <QuestSlider
-              aria-labelledby="discrete-slider"
-              valueLabelDisplay="on"
-              step={1}
-              marks
-              min={1}
-              max={5}
-              value={3}
-              className={classes.scale}
-            />
-          </Grid>
+          <QuestSlider
+            aria-labelledby="discrete-slider"
+            valueLabelDisplay="on"
+            step={1}
+            marks
+            min={1}
+            max={5}
+            value={value}
+            className={`${classes.scale} ${answerClass}`}
+          />
         </div>
       );
     }
 
     if (answerBefore !== undefined || answerAfter !== undefined) {
+      const values = [];
+      let beforeValue = 0;
+      let afterValue = 0;
+      let answerClass = '';
+
+      if (answerBefore) {
+        beforeValue = scaleToNumberMap[answerBefore.option!.optionText!];
+        values.push(beforeValue);
+        answerClass += `is-blue-${beforeValue} has-before`;
+      }
+
+      if (answerAfter) {
+        afterValue = scaleToNumberMap[answerAfter.option!.optionText!];
+        if (afterValue !== beforeValue) {
+          values.push(afterValue);
+        }
+        answerClass += ` is-red-${afterValue} has-after`;
+      }
+
       return (
         <div className={classes.top}>
-          <Grid container alignItems="center" justify="space-between">
-            <Grid
-              item
-              xs={6}
-              style={{ paddingLeft: '0.5rem', borderRight: '2px solid grey' }}
-            >
-              <Grid container justify="center">
-                <Typography variant="h6" style={{ color: '#695F5F' }}>
-                  Before
-                </Typography>
-              </Grid>
-              {answerBefore !== undefined ? (
-                <QuestSlider
-                  aria-labelledby="discrete-slider"
-                  valueLabelDisplay="on"
-                  step={1}
-                  marks
-                  min={1}
-                  max={5}
-                  value={3}
-                  className={classes.scale}
-                />
-              ) : (
-                <Typography
-                  style={{ paddingTop: '1rem', paddingRight: '1rem' }}
-                >
-                  This question was added after the student attempted the
-                  pre-programme questionnaire.
-                </Typography>
-              )}
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              style={{ paddingLeft: '0.5rem', borderRight: '2px solid grey' }}
-            >
-              <Grid container justify="center">
-                <Typography variant="h6" style={{ color: '#695F5F' }}>
-                  After
-                </Typography>
-              </Grid>
-              {answerAfter !== undefined ? (
-                <QuestSlider
-                  aria-labelledby="discrete-slider"
-                  valueLabelDisplay="on"
-                  step={1}
-                  marks
-                  min={1}
-                  max={5}
-                  value={3}
-                  className={classes.scale}
-                />
-              ) : (
-                <Typography
-                  style={{ paddingTop: '1rem', paddingRight: '1rem' }}
-                >
-                  This question has been modified or deleted after the student
-                  attempted the pre-programme questionnaire.
-                </Typography>
-              )}
-            </Grid>
-          </Grid>
+          <QuestSlider
+            aria-labelledby="discrete-slider"
+            valueLabelDisplay="on"
+            step={1}
+            marks
+            min={1}
+            max={5}
+            value={values}
+            className={`${classes.scale} ${answerClass}`}
+          />
         </div>
       );
     }
