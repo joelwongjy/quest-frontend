ca6684fe3a190d501854500863460b80d8b80575
Hanming Zhu <hanming@huawen.com>
2021-03-13 21:20:26 +0800

    fix: fix filter condition and add sorting

--- src/utils/sortingUtils.ts
+++ src/utils/sortingUtils.ts
@@ -1,3 +1,4 @@
+import { PersonListData } from 'interfaces/models/persons';
 import { QuestionData, QuestionPostData } from 'interfaces/models/questions';
 
 export const sortByOrder = <T extends QuestionData | QuestionPostData>(
@@ -5,4 +6,8 @@ export const sortByOrder = <T extends QuestionData | QuestionPostData>(
   b: T
 ): number => {
   return a.order - b.order;
+};
+
+export const sortByName = <T extends PersonListData>(a: T, b: T): number => {
+  return a.name.localeCompare(b.name);
 };
