b1837738809474675e044a91b07705b997595d92
Hanming Zhu <hanming@huawen.com>
2021-03-16 00:10:03 +0800

    refactor: generalise sortingUtils

--- src/utils/sortingUtils.ts
+++ src/utils/sortingUtils.ts
@@ -1,13 +1,10 @@
-import { PersonListData } from 'interfaces/models/persons';
-import { QuestionData, QuestionPostData } from 'interfaces/models/questions';
-
-export const sortByOrder = <T extends QuestionData | QuestionPostData>(
+export const sortByOrder = <T extends { order: number }>(
   a: T,
   b: T
 ): number => {
   return a.order - b.order;
 };
 
-export const sortByName = <T extends PersonListData>(a: T, b: T): number => {
+export const sortByName = <T extends { name: string }>(a: T, b: T): number => {
   return a.name.localeCompare(b.name);
 };
