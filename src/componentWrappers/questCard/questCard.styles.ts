d3b3b66a0e4a8efed99c52e33cf529c47e2fa503
Joel Wong <joelwongjy@gmail.com>
2021-04-06 02:26:04 +0800

    fix: adjust shadow for cards and hover
    Fixes #125

--- src/componentWrappers/questCard/questCard.styles.ts
+++ src/componentWrappers/questCard/questCard.styles.ts
@@ -5,17 +5,17 @@ export const useStyles = makeStyles(() =>
     hover: {
       flexGrow: 1,
       borderRadius: 12,
-      boxShadow: '0 0 20px 0 rgba(0,0,0,0.12)',
+      boxShadow: '0 0 6px 0 rgba(0,0,0,0.08)',
       transition: '0.3s',
       '&:hover': {
-        transform: 'translateY(-3px)',
-        boxShadow: '0 4px 20px 0 rgba(0,0,0,0.12)',
+        transform: 'translateY(-4px)',
+        boxShadow: '0 4px 6px 0 rgba(0,0,0,0.18)',
       },
     },
     root: {
       flexGrow: 1,
       borderRadius: 12,
-      boxShadow: '0 0 20px 0 rgba(0,0,0,0.12)',
+      boxShadow: '0 0 6px 0 rgba(0,0,0,0.08)',
     },
   })
 );
