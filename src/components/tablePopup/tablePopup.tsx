be54e33422051d5dfa4ad6cda29087948eae9430
Joel Wong <joelwongjy@gmail.com>
2021-05-30 02:58:19 +0800

    Add TablePopup Component
    Overflown content is cut off and MUI does not have a way of dynamically
    wrapping content.
    
    Adapted a solution online to create a popover component that will
    display the full content on hover. Also added a function that takes in
    CellParams to render a default tablePopup component.

---
+++ src/components/tablePopup/tablePopup.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Paper, Popper, Typography } from '@material-ui/core';
+import { isOverflown } from '@material-ui/data-grid';
+
+import { useStyles } from './tablePopup.styles';
+
+interface TablePopupProps {
+  value: string;
+  width: number;
+}
+
+const TablePopup: React.FunctionComponent<TablePopupProps> = ({
+  value,
+  width,
+}) => {
+  const wrapper = React.useRef<HTMLDivElement | null>(null);
+  const cellDiv = React.useRef(null);
+  const cellValue = React.useRef(null);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const classes = useStyles();
+  const [showFullCell, setShowFullCell] = React.useState(false);
+  const [showPopper, setShowPopper] = React.useState(false);
+
+  const handleMouseEnter = () => {
+    const isCurrentlyOverflown = isOverflown(cellValue.current!);
+    setShowPopper(isCurrentlyOverflown);
+    setAnchorEl(cellDiv.current);
+    setShowFullCell(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowFullCell(false);
+  };
+
+  React.useEffect(() => {
+    if (!showFullCell) {
+      return undefined;
+    }
+
+    const handleKeyDown = (nativeEvent: KeyboardEvent) => {
+      if (nativeEvent.key === 'Escape' || nativeEvent.key === 'Esc') {
+        setShowFullCell(false);
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [setShowFullCell, showFullCell]);
+
+  return (
+    <div
+      ref={wrapper}
+      className={classes.root}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        ref={cellDiv}
+        style={{
+          height: 1,
+          width,
+          display: 'block',
+          position: 'absolute',
+          top: 0,
+        }}
+      />
+      <div ref={cellValue} className="cellValue">
+        {value}
+      </div>
+      {showPopper && (
+        <Popper
+          open={showFullCell && anchorEl != null}
+          anchorEl={anchorEl}
+          style={{ width, marginLeft: -17 }}
+        >
+          <Paper
+            elevation={1}
+            style={{ minHeight: wrapper.current!.offsetHeight - 3 }}
+          >
+            <Typography variant="body2" style={{ padding: 8 }}>
+              {value}
+            </Typography>
+          </Paper>
+        </Popper>
+      )}
+    </div>
+  );
+};
+
+export default TablePopup;
