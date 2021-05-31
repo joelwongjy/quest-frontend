3e65eba96eea4b8e83f37c252a8802885cc91086
Asthenosphere <58675456+Asthenosphere@users.noreply.github.com>
2021-06-01 00:42:38 +0800

    feat: beautify checkbox and add sorting to programmes

--- src/routes/students/Students.tsx
+++ src/routes/students/Students.tsx
@@ -3,7 +3,14 @@
 import React, { useEffect, useReducer } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
-import { Button, IconButton } from '@material-ui/core';
+import {
+  Button,
+  createMuiTheme,
+  createStyles,
+  IconButton,
+  makeStyles,
+  Theme,
+} from '@material-ui/core';
 import { GridCellParams, GridColDef, GridRowId } from '@material-ui/data-grid';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
@@ -21,7 +28,69 @@ import { RouteState } from 'interfaces/routes/common';
 import ApiService from 'services/apiService';
 import { getAlertCallback } from 'utils/alertUtils';
 
-import { useStyles } from './students.styles';
+function customCheckbox(theme: Theme) {
+  return {
+    '& .MuiCheckbox-root svg': {
+      width: 16,
+      height: 16,
+      backgroundColor: 'transparent',
+      border: `1px solid ${
+        theme.palette.type === 'light' ? '#d9d9d9' : 'rgb(67, 67, 67)'
+      }`,
+      borderRadius: 2,
+    },
+    '& .MuiCheckbox-root svg path': {
+      display: 'none',
+    },
+    '& .MuiCheckbox-root.Mui-checked:not(.MuiCheckbox-indeterminate) svg': {
+      backgroundColor: '#044682',
+      borderColor: '#044682',
+    },
+    '& .MuiCheckbox-root.Mui-checked .MuiIconButton-label:after': {
+      position: 'absolute',
+      display: 'table',
+      border: '2px solid #fff',
+      borderTop: 0,
+      borderLeft: 0,
+      transform: 'rotate(45deg) translate(-50%,-50%)',
+      opacity: 1,
+      transition: 'all .2s cubic-bezier(.12,.4,.29,1.46) .1s',
+      content: '""',
+      top: '50%',
+      left: '39%',
+      width: 5.71428571,
+      height: 9.14285714,
+    },
+    '& .MuiCheckbox-root.MuiCheckbox-indeterminate .MuiIconButton-label:after':
+      {
+        width: 8,
+        height: 8,
+        backgroundColor: '#044682',
+        transform: 'none',
+        top: '39%',
+        border: 0,
+      },
+  };
+}
+
+const defaultTheme = createMuiTheme();
+const useStyles = makeStyles(
+  (theme: Theme) =>
+    createStyles({
+      root: {
+        ...customCheckbox(theme),
+      },
+      button: {
+        margin: theme.spacing(1.5, 0),
+        color: theme.palette.primary.main,
+      },
+      dataGrid: {
+        display: 'flex',
+        height: '100%',
+      },
+    }),
+  { defaultTheme }
+);
 
 interface StudentsState extends RouteState {
   students: PersonListData[];
@@ -165,6 +234,18 @@ const Students: React.FunctionComponent = () => {
       field: 'programmes',
       headerName: 'Programmes',
       width: 200,
+      filterable: false,
+      sortComparator: (v1, v2) => {
+        const lhs = v1 as ProgrammeListData[];
+        const rhs = v2 as ProgrammeListData[];
+        if (lhs.length === 0) {
+          return -1;
+        }
+        if (rhs.length === 0) {
+          return 1;
+        }
+        return lhs[0].name < rhs[0].name ? -1 : 1;
+      },
       renderCell: (params: GridCellParams) => {
         return (
           <TablePopup
@@ -211,7 +292,7 @@ const Students: React.FunctionComponent = () => {
       renderCell: renderDefaultTablePopup,
     },
     {
-      field: '',
+      field: 'actions',
       headerName: 'Actions',
       sortable: false,
       renderCell: (params: GridCellParams) => {
@@ -264,6 +345,7 @@ const Students: React.FunctionComponent = () => {
       <div className={classes.dataGrid}>
         <div style={{ flexGrow: 1 }}>
           <QuestDataGrid
+            className={classes.root}
             loading={state.isLoading}
             rows={state.students}
             columns={columns}
