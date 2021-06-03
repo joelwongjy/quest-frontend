8c3799b46659733b84e7ec0652a26793e026a904
Asthenosphere <58675456+Asthenosphere@users.noreply.github.com>
2021-06-04 00:20:22 +0800

    refator: refactor out quest data grid

--- src/routes/students/Students.tsx
+++ src/routes/students/Students.tsx
@@ -3,14 +3,7 @@
 import React, { useEffect, useReducer } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
-import {
-  Button,
-  createMuiTheme,
-  createStyles,
-  IconButton,
-  makeStyles,
-  Theme,
-} from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
 import { GridCellParams, GridColDef, GridRowId } from '@material-ui/data-grid';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
@@ -28,69 +21,7 @@ import { RouteState } from 'interfaces/routes/common';
 import ApiService from 'services/apiService';
 import { getAlertCallback } from 'utils/alertUtils';
 
-function customCheckbox(theme: Theme) {
-  return {
-    '& .MuiCheckbox-root svg': {
-      width: 16,
-      height: 16,
-      backgroundColor: 'transparent',
-      border: `1px solid ${
-        theme.palette.type === 'light' ? '#d9d9d9' : 'rgb(67, 67, 67)'
-      }`,
-      borderRadius: 2,
-    },
-    '& .MuiCheckbox-root svg path': {
-      display: 'none',
-    },
-    '& .MuiCheckbox-root.Mui-checked:not(.MuiCheckbox-indeterminate) svg': {
-      backgroundColor: '#044682',
-      borderColor: '#044682',
-    },
-    '& .MuiCheckbox-root.Mui-checked .MuiIconButton-label:after': {
-      position: 'absolute',
-      display: 'table',
-      border: '2px solid #fff',
-      borderTop: 0,
-      borderLeft: 0,
-      transform: 'rotate(45deg) translate(-50%,-50%)',
-      opacity: 1,
-      transition: 'all .2s cubic-bezier(.12,.4,.29,1.46) .1s',
-      content: '""',
-      top: '50%',
-      left: '39%',
-      width: 5.71428571,
-      height: 9.14285714,
-    },
-    '& .MuiCheckbox-root.MuiCheckbox-indeterminate .MuiIconButton-label:after':
-      {
-        width: 8,
-        height: 8,
-        backgroundColor: '#044682',
-        transform: 'none',
-        top: '39%',
-        border: 0,
-      },
-  };
-}
-
-const defaultTheme = createMuiTheme();
-const useStyles = makeStyles(
-  (theme: Theme) =>
-    createStyles({
-      root: {
-        ...customCheckbox(theme),
-      },
-      button: {
-        margin: theme.spacing(1.5, 0),
-        color: theme.palette.primary.main,
-      },
-      dataGrid: {
-        display: 'flex',
-        height: '100%',
-      },
-    }),
-  { defaultTheme }
-);
+import { useStyles } from './students.styles';
 
 interface StudentsState extends RouteState {
   students: PersonListData[];
@@ -345,7 +276,6 @@ const Students: React.FunctionComponent = () => {
       <div className={classes.dataGrid}>
         <div style={{ flexGrow: 1 }}>
           <QuestDataGrid
-            className={classes.root}
             loading={state.isLoading}
             rows={state.students}
             columns={columns}
