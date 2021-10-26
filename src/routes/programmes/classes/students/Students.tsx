732f7b28ca1d8f4a3b6fc33cbd33d992acec73dc
Zhu Hanming <45617494+zhuhanming@users.noreply.github.com>
2021-10-26 20:49:51 +0800

    Revert "chore: deprecate CUD on programmes & classes"

--- src/routes/programmes/classes/students/Students.tsx
+++ src/routes/programmes/classes/students/Students.tsx
@@ -1,14 +1,28 @@
 import React, { useEffect, useReducer } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { Button } from '@material-ui/core';
 
 import PageContainer from 'components/pageContainer';
 import PageHeader from 'components/pageHeader';
+import StudentForm from 'components/studentForm';
 import StudentList from 'components/studentList';
 import QuestAlert from 'componentWrappers/questAlert';
-import { CLASSES, HOME, PROGRAMMES, STUDENTS } from 'constants/routes';
+import {
+  ADD,
+  CLASSES,
+  HOME,
+  PERSONS,
+  PROGRAMMES,
+  STUDENTS,
+} from 'constants/routes';
 import { ClassData } from 'interfaces/models/classes';
+import { PersonData, PersonListData } from 'interfaces/models/persons';
+import { StudentMode } from 'interfaces/models/users';
 import { ClassRouteParams, RouteState } from 'interfaces/routes/common';
 import ApiService from 'services/apiService';
+import { getAlertCallback } from 'utils/alertUtils';
+
+import { useStyles } from './students.styles';
 
 interface StudentsState extends RouteState {
   questClass: ClassData | null;
@@ -16,6 +30,8 @@ interface StudentsState extends RouteState {
   closeHandler: () => void;
   confirmHandler: () => void;
   cancelHandler: undefined | (() => void);
+  isEditing: boolean;
+  selectedStudent: PersonData | undefined;
 }
 
 const Students: React.FunctionComponent = () => {
@@ -40,8 +56,11 @@ const Students: React.FunctionComponent = () => {
       cancelHandler: () => {
         setState({ isAlertOpen: false });
       },
+      isEditing: false,
+      selectedStudent: undefined,
     }
   );
+  const classes = useStyles();
 
   useEffect(() => {
     let didCancel = false;
@@ -103,10 +122,87 @@ const Students: React.FunctionComponent = () => {
     },
   ];
 
+  const alertCallback = getAlertCallback(setState);
+
+  const handleEdit = async (student: PersonListData) => {
+    setState({ isLoading: true });
+    try {
+      const response = await ApiService.get(`${PERSONS}/${student.id}`);
+      if (response.status === 200) {
+        setState({
+          isEditing: true,
+          selectedStudent: response.data.person as PersonData,
+          isLoading: false,
+        });
+      }
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.log(e);
+      setState({ isLoading: false, isError: true });
+    }
+  };
+
+  const handleDelete = (student: PersonListData) => {
+    const { questClass } = state;
+    if (questClass == null) {
+      return;
+    }
+
+    alertCallback(
+      true,
+      true,
+      'Are you sure?',
+      'You will not be able to retrieve deleted students.',
+      () => {
+        // TODO: Add error handling to deletion
+        ApiService.delete(`${STUDENTS}`, {
+          data: {
+            persons: [student.id],
+          },
+        });
+        const index = questClass.students.indexOf(student);
+        const newStudents = questClass.students.slice();
+        newStudents.splice(index, 1);
+        setState({
+          questClass: { ...questClass, students: newStudents },
+        });
+      },
+      undefined
+    );
+  };
+
   return (
     <PageContainer>
-      <PageHeader breadcrumbs={breadcrumbs} />
-      <StudentList students={state.questClass?.students ?? []} />
+      <PageHeader
+        breadcrumbs={breadcrumbs}
+        action={
+          state.isEditing ? undefined : (
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              component={Link}
+              to={`${PROGRAMMES}/${id}${CLASSES}/${classId}${STUDENTS}${ADD}`}
+            >
+              Add Students
+            </Button>
+          )
+        }
+      />
+      {state.isEditing ? (
+        <StudentForm
+          mode={StudentMode.EDIT}
+          student={state.selectedStudent}
+          alertCallback={alertCallback}
+          cancelCallback={() => setState({ isEditing: false })}
+        />
+      ) : (
+        <StudentList
+          students={state.questClass?.students ?? []}
+          editCallback={handleEdit}
+          deleteCallback={handleDelete}
+        />
+      )}
       <QuestAlert
         isAlertOpen={state.isAlertOpen!}
         hasConfirm={state.hasConfirm}
